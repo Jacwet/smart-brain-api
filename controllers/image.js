44439b26646aff147181781a5b063685475e1d22
Jacqueline Wettergren <jacqueline.wettergren@gmail.com>
2024-01-18 16:13:12 +0100

    230118

--- controllers/image.js
+++ controllers/image.js
@@ -10,6 +10,7 @@ const handleApiCall = (req, res) => {
   app.models.predict('face-detection', req.body.input)
     .then(data => {
       res.json(data);
+      console.log(data);
     })
     .catch(err => res.status(400).json('unable to work with API'))
 }
